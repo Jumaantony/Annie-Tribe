0627d13f43de3961fba825d587054f8196526540
jumaantony <jumaantony840@gmail.com>
2022-10-04 09:54:08 +0300

    made some tweaks

--- annie/annietribe/urls.py
+++ annie/annietribe/urls.py
@@ -19,8 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin_tools_stats/', include('admin_tools_stats.urls')),
-
                   path('admin/', admin.site.urls),
 
                   # orders urls
